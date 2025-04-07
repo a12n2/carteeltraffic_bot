d222d89fd0ce564b0f9d9ccd21567bb152c4c0df
a12n2 <pordandrej@gmail.com>
2025-04-07 17:18:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,20 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "7854283477:AAFPqBOplPvUN8yaiWNOXYf6Gre6-hT4YuY"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет!")
+    await update.message.reply_text("👋 Добро пожаловать в команду по арбитражу трафика!
+Ты только что сделал шаг в мир, где каждый клик — это возможность, а каждая связка — путь к прибыли.
 
+🚀 Здесь ты получишь:
+— Поддержку от команды 24/7
+— Актуальные офферы и проверенные схемы
+— Гайды, инсайды и рабочие стратегии
+— И, конечно, атмосферу роста и движения к результату
+
+⚡️ Готов вливаться в трафик и зарабатывать? Погнали!")
+    
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
